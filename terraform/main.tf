f39af4c5fbbf98bdd77a815e784072b92966d29e
Antonio Ruiz <toninoes@gmail.com>
2022-08-04 18:59:30 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,14 @@
 provider "aws" {
-  region  = var.region
-  #profile = var.profile
+  region = var.region
 }
 
 terraform {
   required_version = "~> 1.0.11"
 
   backend "s3" {
-    bucket  = "toninoes-aws2-infra-tf-state"
-    key     = "github-actions-terraform/terraform.tfstate"
-    region  = "eu-west-1"
-    profile = "toninoes-aws2"
+    bucket = "toninoes-aws2-infra-tf-state"
+    key    = "github-actions-terraform/terraform.tfstate"
+    region = "eu-west-1"
   }
 
   required_providers {
