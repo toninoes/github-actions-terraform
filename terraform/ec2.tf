6dd777cef00160e4eab37ec39f32ca27598de204
Antonio Ruiz <toninoes@gmail.com>
2022-08-05 11:40:58 +0200

    Update ec2.tf
    change to NANO

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -16,7 +16,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.small"
+  instance_type = "t3.nano"
 
   tags = {
     Name = "HelloWorld"
